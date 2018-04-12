25468880d2874dca426395bd37428cf7981b4511
Chris <chrishjimenez@gmail.com>
2018-04-11 20:41:11 -0400

    Filled in GET info

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 var express = require("express");
 var app = express();
+
+app.get("/", function(req, res){
+  res.send("Hi there!");
+});
+
+app.get("/bye", function(req, res){
+  res.send("Peace out.")
+});
+
+app.get("/dog", function(req, res){
+  res.send("Bark?");
+});
+
+app.listen(3000, 'localhost', function(){
+  console.log("Server has started.")
+});
